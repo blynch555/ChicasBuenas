eb2e8f6e7ea0be65594559832213b4143cbb7793
JOse Eduardo Rios <kattatzu@gmail.com>
2015-02-13 20:58:14 +0000

    lol

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -37,16 +37,6 @@
 				<a class="twitter-timeline" href="https://twitter.com/chicasbuenascl" data-widget-id="547284619255898112">Tweets por el @chicasbuenascl.</a>
 				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 			</div>
-			<div style="margin-top: 10px;">
-				<div id="fb-root"></div>
-				<script>(function(d, s, id) {
-				  var js, fjs = d.getElementsByTagName(s)[0];
-				  if (d.getElementById(id)) return;
-				  js = d.createElement(s); js.id = id;
-				  js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&appId=430700113754492&version=v2.0";
-				  fjs.parentNode.insertBefore(js, fjs);
-				}(document, 'script', 'facebook-jssdk'));</script>
-			</div>
 		</div>
 	</div>
 
