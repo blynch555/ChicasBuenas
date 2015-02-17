7a25b1e5b307bfdcf852e57dff811934743f5a25
José Eduardo Ríos <kattatzu@gmail.com>
2015-02-17 02:34:32 -0300

    xD

--- app/views/templates/default.blade.php
+++ app/views/templates/default.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<title>@yield('title', trans('site.title'))</title>
 
-	<link rel="icon" type="image/png" href="{{ asset('img/fb_logo.png') }}" />
+	<link rel="icon" type="image/png" href="{{ asset('img/icon.png') }}" />
 
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
