e0815de759dccaa681f6b06105e763b78713900b
José Eduardo Ríos <kattatzu@gmail.com>
2015-02-20 21:23:18 -0300

    xD

--- app/views/chicas/profile.blade.php
+++ app/views/chicas/profile.blade.php
@@ -151,7 +151,7 @@
 	<script>
 	var escort = {
 		name: '{{ str_replace("'", '"', $escort->name) }}',
-		description: '{{ Str::slug($escort->description, ' ') }}',
+		description: '{{ Str::limit(Str::slug($escort->description, ' '), 100) }}',
 		photo_url: '{{ $escort->thumb() }}',
 		url: '{{ $escort->shortUrl() }}'
 	};
