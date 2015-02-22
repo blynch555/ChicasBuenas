63014f2d26a313fcfa3416e7f3af5fdd409b81b2
José Eduardo Ríos <kattatzu@gmail.com>
2015-02-21 22:23:31 -0300

    xD

--- public/js/chicas/profile.js
+++ public/js/chicas/profile.js
@@ -1,6 +1,26 @@
 $(function(){
 	$(".escortPhotos").slick({
-		slidesToShow: 1
+		slidesToShow: 1,
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					arrows: false,
+					centerMode: true,
+					centerPadding: '40px',
+					slidesToShow: 4
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					arrows: false,
+					centerMode: true,
+					centerPadding: '40px',
+					slidesToShow: 4
+				}
+			}
+		]
 	});
 
 	$('.escortPhotos').magnificPopup({
